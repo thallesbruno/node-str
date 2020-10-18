f9a2196042c9145b93765e85bcf81858e4779dc4
Thalles Bruno <thallesbgns@gmail.com>
2020-10-18 19:44:37 -0300

    crud

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 const router = express.Router(); //rotas da aplicação
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
+//Carrega as rotas
+const index.
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 router.post('/post', (req, res, next) => {
-    res.status(201).send({
+    res.status(202).send({
         title: "Testando o POST",
         version: "0.0.1",
         info: "Criando..."
@@ -28,6 +29,25 @@ router.put('/put', (req, res, next) => {
     });
 });
 
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
