2ae25aa319fe4d48a53a83b1076597498f62c853
Thalles Bruno <thallesbgns@gmail.com>
2020-10-18 21:06:04 -0300

    reestruturacao

--- src/app.js
+++ src/app.js
@@ -3,51 +3,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const app = express();
 const router = express.Router(); //rotas da aplicação
 
 //Carrega as rotas
-const index.
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-router.post('/post', (req, res, next) => {
-    res.status(202).send({
-        title: "Testando o POST",
-        version: "0.0.1",
-        info: "Criando..."
-    });
-});
-
-router.put('/put', (req, res, next) => {
-    res.status(428).send({
-        title: "Testando o PUT",
-        version: "0.0.1",
-        info: "Alterando por PUT"
-    });
-});
-
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
