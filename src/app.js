99698dc579bd6fe8dc790ea68189776f5e739e27
Thalles Bruno <thallesbgns@gmail.com>
2020-10-27 11:33:52 -0300

    config.js

--- src/app.js
+++ src/app.js
@@ -5,13 +5,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const config = require('./config');
+
 const app = express();
 const router = express.Router(); //rotas da aplicação
 
 //Conecta ao banco
-mongoose.connect(
-    'mongodb+srv://bruno:bruno@ndstr.6pwh5.azure.mongodb.net/<dbname>?retryWrites=true&w=majority'
-);
+mongoose.connect(config.connectionString);
 
 //Carrega os models
 const Product = require('./models/product');
