e13ed60dfd71518ed8e7c45c825022c4dac39515
Thalles Bruno <thallesbgns@gmail.com>
2020-10-27 08:07:19 -0300

    models order

--- src/app.js
+++ src/app.js
@@ -8,11 +8,15 @@ const mongoose = require('mongoose');
 const app = express();
 const router = express.Router(); //rotas da aplicação
 
+//Conecta ao banco
 mongoose.connect(
     'mongodb+srv://bruno:bruno@ndstr.6pwh5.azure.mongodb.net/<dbname>?retryWrites=true&w=majority'
 );
 
+//Carrega os models
 const Product = require('./models/product');
+const Costumer = require('./models/costumer');
+const Order = require('./models/order');
 
 //Carrega as rotas
 const indexRoute = require('./routes/index-route');
