c9949e9053ceee17c584a63c5fa14c04ee0d434c
Thalles Bruno <thallesbgns@gmail.com>
2020-11-01 22:31:45 -0300

    refresh authentication

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -97,3 +97,37 @@ exports.authenticate = async(req, res, next) => {
         });
     };
 };
+
+
+exports.refreshToken = async(req, res, next) => {
+    try {
+        const token = req.body.token || req.query.token || req.headers['x-access-token'];
+        const data = await authService.decodeToken(token);
+
+        const customer = await repository.getById(data.id);
+
+        if (!customer) {
+            res.status(404).send({message: 'Cliente não encontrado' });
+            return;
+        }
+
+        const tokenData = await authService.generateToken({
+            id: customer._id,
+            email: customer.email,
+            name: customer.name
+        });
+
+        res.status(201).send({
+            token: token,
+            data: {
+                email: customer.email,
+                name: customer.name
+            }
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição: ' + e
+        });
+    };
+};
