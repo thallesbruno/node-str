fecbbfedbfae0f10314e33cc1a3d88ead9be966f
Thalles Bruno <thallesbgns@gmail.com>
2020-11-02 21:27:53 -0300

    isAdmin metodo post de product

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -35,7 +35,8 @@ exports.post = async(req, res, next) => {
         await repository.create({
             name: req.body.name,
             email: req.body.email,
-            password: md5(req.body.password + global.SALT_KEY)
+            password: md5(req.body.password + global.SALT_KEY),
+            roles: ["user"]
         });
 
         emailService.send(
@@ -81,14 +82,16 @@ exports.authenticate = async(req, res, next) => {
         const token = await authService.generateToken({
             id: customer._id,
             email: customer.email,
-            name: customer.name
+            name: customer.name,
+            roles: customer.roles
         });
 
         res.status(201).send({
             token: token,
             data: {
                 email: customer.email,
-                name: customer.name
+                name: customer.name,
+                roles: customer.roles
             }
         });
     } catch (e) {
@@ -114,14 +117,16 @@ exports.refreshToken = async(req, res, next) => {
         const tokenData = await authService.generateToken({
             id: customer._id,
             email: customer.email,
-            name: customer.name
+            name: customer.name,
+            roles: customer.roles
         });
 
         res.status(201).send({
             token: token,
             data: {
                 email: customer.email,
-                name: customer.name
+                name: customer.name,
+                roles: customer.roles
             }
         });
     } catch (e) {
